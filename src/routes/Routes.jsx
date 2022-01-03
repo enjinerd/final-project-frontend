701485f3e8666c664fa75c49d54c609001a2dcc8
enjinerd <dev@ron.my.id>
2022-01-03 13:00:08 +0700

    feat(routes, page): vaccination routing and fix bottom nav route url

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -7,12 +7,12 @@ import {
   useParams,
 } from "react-router-dom";
 
-import { Home, NotFound} from "pages";
+import { Home, NotFound } from "pages";
 import { LayoutRoot } from "components/layout/layout-root";
 import { LayoutAdmin } from "components/layout/layout-admin";
 import User from "./User";
+import Vaccination from "./Vaccination";
 import AdminTable from "../components/admin-table/AdminTable";
-
 
 export default function Routes() {
   return (
@@ -24,16 +24,17 @@ export default function Routes() {
           </LayoutRoot>
         </Route>
         <Route path="/user" component={User} />
+        <Route path="/vaccination" component={Vaccination} />
         <Route exact path="/admin/vaccine">
           <LayoutAdmin>
             <AdminTable
               columns={[
-                { title: 'Name', field: 'name' },
-                { title: 'Stock', field: 'stock', type: 'numeric' }
+                { title: "Name", field: "name" },
+                { title: "Stock", field: "stock", type: "numeric" },
               ]}
               data={[
-                { name: 'Sinovac', stock: 600},
-                { name: 'Moderna', stock: 600},
+                { name: "Sinovac", stock: 600 },
+                { name: "Moderna", stock: 600 },
               ]}
             />
           </LayoutAdmin>
