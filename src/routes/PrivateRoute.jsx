6248bec402f88408ab021f53fd0af651095df11c
Ron <35429278+enjinerd@users.noreply.github.com>
2022-01-09 23:58:09 +0700

    fix(app): :bug: change logic to avoid rerender error
    rewrite expired token for auth logic
    
    Error rerender when token is invalid

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -6,24 +6,20 @@ import useAuthStore from "stores/useAuthStore";
 export default function PrivateRoute(props) {
   const { isAuthenticated, isAuthenticating, token, logout } = useAuthStore();
   const [isExpired, setExpired] = useState(false);
-  if (token != null) {
-    const decode = jwt_decode(token);
-    if (decode.exp * 1000 < Date.now()) {
-      setExpired(true);
-      logout();
-    }
-  }
+  const decode = jwt_decode(token);
+
   const { component: Component, ...rest } = props;
   if (isAuthenticating) {
     return (
-      <div className="flex justify-center items-center p-12">
-        <div className="w-20 h-20 rounded-full border-b-2 border-gray-900 animate-spin"></div>
+      <div className="flex items-center justify-center p-12">
+        <div className="w-20 h-20 border-b-2 border-gray-900 rounded-full animate-spin"></div>
       </div>
     );
   }
-  if (isAuthenticated && !isExpired) {
-    return <Route {...rest} render={(props) => <Component {...props} />} />;
+  if (!isAuthenticated && decode.exp * 1000 < Date.now()) {
+    return <Redirect to="/user/login" />;
   }
+  return <Route {...rest} render={(props) => <Component {...props} />} />;
+
   //redirect if there is no user
-  return <Redirect to="/user/login" />;
 }
